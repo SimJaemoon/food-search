23bf18808f2647c310f689ac09c9f733fc741469
SimJaemoon <limun1324@gmail.com>
2024-07-24 22:46:51 +0900

    UI: Create Splash page

---
+++ src/app/Landing/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <header>header region</header>
+      <main className="text-body-lg relative">
+        <span>hello, world!</span>
+      </main>
+    </>
+  );
+}
