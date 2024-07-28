9346379ec6eeafac993d1ca9ce0582e72173a948
SimJaemoon <limun1324@gmail.com>
2024-07-28 19:39:38 +0900

    UI: Create ProductList page

--- src/app/Landing/page.tsx
+++ src/app/Landing/page.tsx
@@ -1,56 +1,63 @@
 import ProductCategroyBox from '@/components/ProductCategoryBox';
 
+// TODO: 개발용 Link 제거하기
+import Link from 'next/link';
+
 export default function Landing() {
   return (
     <>
       <header className="h-[80px] w-full bg-[#348560]">header area</header>
+
       <main className="relative h-[calc(100%-128px)] w-full">
-        {/* 배경 */}
-        <div className="h-full w-full">
-          {/* 상단 */}
-          <div className="h-[36.5%] w-full bg-[#FFD159]"></div>
-          {/* 하단 */}
-          <div className="h-[63.5%] w-full bg-[#74FF8A]"></div>
-        </div>
-        {/* 상품 카테고리 목록 */}
-        <ol
-          className="absolute left-[0] top-[0] z-10 h-full w-full"
-          aria-label="상품 카테고리 목록"
-        >
-          {/* first row */}
-          <li className="absolute left-[0] top-[0] h-[43.5%] w-full">
-            <ol>
-              <li>
-                <ProductCategroyBox rowOrder="first" direction="left" />
-              </li>
-              <li>
-                <ProductCategroyBox rowOrder="first" direction="right" />
-              </li>
-            </ol>
-          </li>
-          {/* second row */}
-          <li className="absolute left-[0] top-[30%] h-[28%] w-full">
-            <ol>
-              <li>
-                <ProductCategroyBox rowOrder="second" direction="left" />
-              </li>
-              <li>
-                <ProductCategroyBox rowOrder="second" direction="right" />
-              </li>
-            </ol>
-          </li>
-          {/* third row */}
-          <li className="absolute left-[0] top-[66.5%] h-[33.5%] w-full">
-            <ol>
-              <li>
-                <ProductCategroyBox rowOrder="third" direction="left" />
-              </li>
-              <li>
-                <ProductCategroyBox rowOrder="third" direction="right" />
-              </li>
-            </ol>
-          </li>
-        </ol>
+        {/* TODO: 개발용 Link 제거하기 */}
+        <Link href={'/ProductList'}>
+          {/* 배경 */}
+          <div className="h-full w-full">
+            {/* 상단 */}
+            <div className="h-[36.5%] w-full bg-[#FFD159]"></div>
+            {/* 하단 */}
+            <div className="h-[63.5%] w-full bg-[#74FF8A]"></div>
+          </div>
+          {/* 상품 카테고리 목록 */}
+          <ol
+            className="absolute left-[0] top-[0] z-10 h-full w-full"
+            aria-label="상품 카테고리 목록"
+          >
+            {/* first row */}
+            <li className="absolute left-[0] top-[0] h-[43.5%] w-full">
+              <ol>
+                <li>
+                  <ProductCategroyBox rowOrder="first" direction="left" />
+                </li>
+                <li>
+                  <ProductCategroyBox rowOrder="first" direction="right" />
+                </li>
+              </ol>
+            </li>
+            {/* second row */}
+            <li className="absolute left-[0] top-[30%] h-[28%] w-full">
+              <ol>
+                <li>
+                  <ProductCategroyBox rowOrder="second" direction="left" />
+                </li>
+                <li>
+                  <ProductCategroyBox rowOrder="second" direction="right" />
+                </li>
+              </ol>
+            </li>
+            {/* third row */}
+            <li className="absolute left-[0] top-[66.5%] h-[33.5%] w-full">
+              <ol>
+                <li>
+                  <ProductCategroyBox rowOrder="third" direction="left" />
+                </li>
+                <li>
+                  <ProductCategroyBox rowOrder="third" direction="right" />
+                </li>
+              </ol>
+            </li>
+          </ol>
+        </Link>
       </main>
       <footer className="h-12 w-full bg-[#475bce]">footer area</footer>
     </>
--- src/app/ProductList/page.tsx
+++ src/app/ProductList/page.tsx
@@ -1,3 +1,31 @@
 export default function ProductList() {
-  return <div>hello, world!</div>;
+  return (
+    <>
+      <header className="h-12 w-full bg-[#348560]">header area</header>
+      <nav className="h-[80px] w-full">
+        <div className="h-8 w-full bg-[#85344c]">nav area 1</div>
+        <div className="h-12 w-full bg-[#af4f7fc9]">nav area 2</div>
+      </nav>
+      <main className="relative h-[calc(100%-176px)] w-full">
+        {/* 매대 상자 */}
+        <div className="absolute bottom-4 left-[12px] h-[148px] w-[calc(100%-24px)]">
+          {/* 윗면 */}
+          <div className="absolute left-[0] top-[0] h-11 w-full bg-background [clip-path:polygon(0%_0%,_calc(100%-24px)_0%,_100%_44px,_12px_44px)]">
+            {/* 외곽선 생성 */}
+            <div className="absolute left-[0.5px] top-[0.5px] h-[calc(100%-1px)] w-[calc(100%-1px)] bg-secondaryEmphasize [clip-path:polygon(0%_0%,_calc(100%-24px)_0%,_100%_44px,_12px_44px)]"></div>
+          </div>
+          {/* 정면 */}
+          <div className="absolute left-[12px] top-[44px] h-[calc(100%-44px)] w-[calc(100%-12px)] border-[0.5px] border-background bg-secondary shadow-figma"></div>
+          {/* 측면 */}
+          <div className="absolute left-[0] top-[0] h-full w-3 bg-background [clip-path:polygon(0%_0%,_100%_44px,_100%_100%,_0%_calc(100%-56px))]">
+            {/* 외곽선 생성 */}
+            <div className="absolute left-[0.5px] top-[0.5px] h-[calc(100%-1px)] w-[calc(100%-1px)] bg-secondary [clip-path:polygon(0%_0%,_100%_44px,_100%_100%,_0%_calc(100%-56px))]"></div>
+          </div>
+        </div>
+        {/* 상품 목록 */}
+        <ol className="absolute left-[0] top-[0] h-[calc(100%-16px-148px+28px)] w-full bg-highlight opacity-50"></ol>
+      </main>
+      <footer className="h-12 w-full bg-[#475bce]">footer area</footer>
+    </>
+  );
 }
