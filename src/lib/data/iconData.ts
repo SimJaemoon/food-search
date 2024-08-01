4179b611e32a5b65a45adfdede0d84a87729bbf5
SimJaemoon <limun1324@gmail.com>
2024-08-02 01:35:54 +0900

    UI: Add icon image files and Update iconData.ts

---
+++ src/lib/data/iconData.ts
@@ -0,0 +1,66 @@
+export type IconName = keyof typeof iconData;
+export const iconData = {
+  delivery: {
+    url: '/Delivery',
+    text: '배송조회',
+    src: '/icon/delivery.png',
+    width: 24,
+    height: 24,
+  },
+  home: {
+    url: '/',
+    text: '입구로 이동',
+    src: '/icon/home.png',
+    width: 40,
+    height: 34,
+  },
+  cart: {
+    url: '/Cart',
+    text: '장바구니',
+    src: '/icon/cart.png',
+    width: 24,
+    height: 24,
+  },
+  person: {
+    url: '/My',
+    text: '내 정보',
+    src: '/icon/person.png',
+    width: 24,
+    height: 24,
+  },
+  noticeBubble: {
+    url: '/Notice',
+    text: '알림',
+    src: '/icon/noticeBubble.png',
+    width: 24,
+    height: 24,
+  },
+  back: {
+    url: '',
+    text: '뒤로가기',
+    src: '/icon/west.png',
+    width: 20,
+    height: 14,
+  },
+  close: {
+    url: '',
+    text: '닫기',
+    src: '/icon/close.png',
+    width: 12,
+    height: 12,
+  },
+  closeCircle: {
+    url: '',
+    text: '지우기',
+    src: '/icon/closeCircle.png',
+    width: 16,
+    height: 16,
+  },
+  magnifier: {
+    url: '',
+    text: '검색하기',
+    src: '/icon/magnifier.png',
+    width: 24,
+    height: 24,
+  },
+};
