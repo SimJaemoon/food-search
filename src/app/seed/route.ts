3ee4a410afe6e527ebfcf3fb7b5170a1e238e08e
SimJaemoon <limun1324@gmail.com>
2024-09-05 22:14:11 +0900

    feat: Update seed route.ts : product data

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -1,4 +1,5 @@
 import { mockProductCategoryData } from '@/lib/data/dataset/mockProductCategoryData';
+import { mockProductData } from '@/lib/data/dataset/mockProductData';
 
 import { db } from '@vercel/postgres';
 
@@ -46,10 +47,45 @@ async function seedProductCategory() {
   return insertedProductCategory;
 }
 
+async function seedProduct() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS product (
+      product_id VARCHAR(255) PRIMARY KEY,
+      brand_id VARCHAR(255) NOT NULL,
+      brand_name VARCHAR(255) NOT NULL,
+      image_url VARCHAR(255) NOT NULL,
+      product_name VARCHAR(255) NOT NULL,
+      product_price INT NOT NULL,
+      quantity_unit VARCHAR(255) NOT NULL,
+      quantity_value INT NOT NULL,
+      review_average_score REAL NOT NULL,
+      review_number INT NOT NULL,
+      product_origin VARCHAR(255) NOT NULL,
+      product_description TEXT NOT NULL,
+      seller_id VARCHAR(255) NOT NULL,
+      seller_name VARCHAR(255) NOT NULL
+    );
+  `;
+
+  const insertedProduct = await Promise.all(
+    mockProductData.map((v) => {
+      return client.sql`
+              INSERT INTO product (product_id, brand_id, brand_name, image_url, product_name, product_price, quantity_unit, quantity_value, review_average_score, review_number, product_origin, product_description, seller_id, seller_name)
+              VALUES (${v.product_id}, ${v.brand_id}, ${v.brand_name}, ${v.image_url}, ${v.product_name}, ${v.product_price}, ${v.quantity_unit}, ${v.quantity_value}, ${v.review_average_score}, ${v.review_number}, ${v.product_origin}, ${v.product_description}, ${v.seller_id}, ${v.seller_name})
+              ON CONFLICT (product_id) DO NOTHING;
+            `;
+    }),
+  );
+
+  return insertedProduct;
+}
+
 export async function GET() {
   try {
     await client.sql`BEGIN`;
-    await seedProductCategory();
+    // await seedProductCategory();
+    // await seedProduct();
     await client.sql`COMMIT`;
 
     return Response.json({ message: 'Database seeded successfully' });
