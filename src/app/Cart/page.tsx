3eba4f8b1c8b83a0b49717dfea2a22ff96724cec
SimJaemoon <limun1324@gmail.com>
2024-09-08 22:16:35 +0900

    UI: Update Cart page : overflow-clip and z-index

--- src/app/Cart/page.tsx
+++ src/app/Cart/page.tsx
@@ -1,11 +1,14 @@
+import Link from 'next/link';
 import CartReceiptContainer from '@/components/containers/CartReceiptContainer';
 
 export default function Cart() {
   return (
     <>
-      <main className="h-[calc(100%-96px)] w-full overflow-hidden">
+      <main className="h-[calc(100%-96px)] w-full">
         {/* delivery information */}
-        <div className="relative z-10">
+        {/* HOLD: 자식 div("00.00(수) 00시 ~ 00시에 배송 가능 (0일 뒤)")의 하단 shadow가 잘리지 않기 위해 overflow-x-hidden을 적용했지만 y축도 hidden, So overflow-x-clip 사용 */}
+        {/* > 스크롤바 생성과 관련된 문제일수도, 현재는 제대로 이해하지 못 함*/}
+        <div className="relative z-10 overflow-x-clip">
           <div className="flex h-10 w-full border-b-[1px] border-secondary bg-background">
             <div className="flex w-[45%] items-center pl-3">
               <div className="text-label-sm whitespace-nowrap pr-1">
@@ -36,15 +39,20 @@ export default function Cart() {
                 {'서울특별시 동대문구 000로 123-00000000'}
               </div>
               <div className="line-clamp-1 w-full break-all">
-                {'000아파트 000동 000호 가나다라마바사'}
+                {'000아파트 000동 000호 텍스트텍스트텍스트'}
               </div>
             </div>
-            <button className="text-label-sm flex h-full w-1/5 items-center justify-center break-keep bg-secondary text-center text-onSecondary shadow-2">
-              {/* TODO: 배송지 변경 Link로 수정하기 */}
-              {'배송지 변경'}
+            <button className="text-label-sm h-full w-1/5 break-keep bg-secondary text-center text-onSecondary shadow-2">
+              {/* TODO: 배송지 변경 btn Link-href 수정하기 */}
+              <Link
+                className="flex h-full w-full items-center justify-center"
+                href={'/EditAddress'}
+              >
+                {'배송지 변경'}
+              </Link>
             </button>
           </div>
-          <div className="relative z-10 h-[40px] w-full border-b-[2px] border-secondary bg-background text-center leading-[40px] shadow-1">
+          <div className="relative h-[40px] w-full border-b-[2px] border-secondary bg-background text-center leading-[40px] shadow-1">
             {/* TODO: 데이터 주입 */}
             {'00.00(수) 00시 ~ 00시에 배송 가능 (0일 뒤)'}
           </div>
